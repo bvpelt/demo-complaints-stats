4c2b074c16a7fb56657adf02d46517d6b5d5b254
Bart van Pelt <bart.vanpelt@gmail.com>
2017-02-03 21:34:05 +0100

    Working example

---
+++ src/main/java/com/example/ComplaintFileEvent.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import java.io.Serializable;
+
+/**
+ * Created by bvpelt on 1/28/17.
+ */
+public class ComplaintFileEvent {
+    private final String id;
+    private final String company;
+    private final String description;
+
+    public ComplaintFileEvent(String id, String company, String description) {
+        this.id = id;
+        this.company = company;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
